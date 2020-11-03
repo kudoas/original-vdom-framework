f697a6f001823bafbaeb197a48213c0e713fd615
Daichi Kudo <sakanactor624@gmail.com>
2020-11-03 23:31:58 +0900

    リアルDOMへの反映

--- src/framework/view.ts
+++ src/framework/view.ts
@@ -18,16 +18,48 @@ export interface View<State, Action> {
 
 /** vdom */
 export interface VNode {
-  nodeName: keyof HTMLElementTagNameMap | SVGElementTagNameMap;
+  nodeName: keyof HTMLElementTagNameMap;
   attributes: Attributes;
   children: NodeType[];
 }
 
 /** vdomを作成する関数 */
-export function h(
-  nodeName: keyof HTMLElementTagNameMap | SVGElementTagNameMap,
+export const h = (
+  nodeName: keyof HTMLElementTagNameMap,
   attributes: Attributes,
   ...children: NodeType[]
-): VNode {
+): VNode => {
   return { nodeName, attributes, children };
-}
+};
+
+/** リアルDOMを生成する */
+export const createElement = (node: NodeType): HTMLElement | Text => {
+  if (!isVNode(node)) {
+    return document.createTextNode(node.toString());
+  }
+  const el = document.createElement(node.nodeName);
+  setAttributes(el, node.attributes);
+  node.children.forEach((child) => el.appendChild(createElement(child)));
+};
+
+const isVNode = (node: NodeType): node is VNode => {
+  return typeof node !== "string" && typeof node !== "number";
+};
+
+/** targetに属性を設定する */
+/** attr: {"class": "hoge", "onclick": function} */
+const setAttributes = (target: HTMLElement, attrs: Attributes) => {
+  for (let attr in attrs) {
+    if (isEventAttr(attr)) {
+      const eventName = attr.slice(2);
+      target.addEventListener(eventName, attrs[attr] as EventListener);
+    } else {
+      target.setAttribute(attr, attrs[attr] as string);
+    }
+  }
+};
+
+/** onがついてたらイベント */
+const isEventAttr = (attr: string): boolean => {
+  return /^on/.test(attr);
+};
